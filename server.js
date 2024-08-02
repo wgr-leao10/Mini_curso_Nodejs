230684c6dcc729de1da108e874c5db4745edf84c
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-08-02 13:46:54 -0300

    database
    correção

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { DatabasePostgres } from "./database_postgres.js"
 const server = fastify()
 
 //  const database = new DatabaseMemory()
-const database = new DatabasePostgres
+const database = new DatabasePostgres()
 
 server.post('/videos', async (request, reply) => {
     const {title, description, duration} = request.body
